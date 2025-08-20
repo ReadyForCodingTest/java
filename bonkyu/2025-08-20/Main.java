fc3fb7db41add4948a86bd18866caadb6ed57e72
lbk00 <lbk6661@gmail.com>
2025-08-20 09:31:29 +0900

    20250820 프로그래머스 레벨1 삼총사

---
+++ bonkyu/2025-08-20/Main.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[] number) {
+        int answer = 0;
+
+        for(int i =0 ; i < number.length-2; i++ ) {
+            for(int k = i+1 ; k < number.length-1; k++ ) {
+                for(int j = k+1 ; j < number.length; j++ ) {
+                    if(number[i] + number[k] + number[j] == 0) answer++;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
+// 3개 선택해서 합 -> 0 ++1
