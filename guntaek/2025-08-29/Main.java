fc16898388677031224960f4d140969e024e2e01
guntaek <rjsxor7584@naver.com>
2025-08-29 08:48:19 +0900

    프로그래머스 레벨1 두 개 뽑아서 더하기

---
+++ guntaek/2025-08-29/Main.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] numbers) {
+        int[] answer;
+
+        Set<Integer> set = new TreeSet<>();
+
+        for (int i = 0; i < numbers.length - 1; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                set.add(numbers[i] + numbers[j]);
+            }
+        }
+
+        answer = new int[set.size()];
+
+        Iterator<Integer> itr = set.iterator();
+
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = itr.next();
+        }
+
+        return answer;
+    }
+}
