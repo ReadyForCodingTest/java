9d77b00d5c781d369108967b2978a282a2d6fe71
GihwanPark <pgh5121@naver.com>
2025-10-15 18:13:51 +0900

    백준 : 1766,2252 - 위상 정렬

---
+++ gihwan/2025-10-13/Baekjun_2252.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Baekjun_2252 {
+    static int N;
+    static int M;
+    static List<List<Integer>> graph = new ArrayList<>();
+    static int[] degree;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        degree = new int[N + 1];
+
+        for (int i = 0; i <= N; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            graph.get(a).add(b);
+            degree[b]++;
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 1; i <= N; i++) {
+            if (degree[i] == 0) q.add(i);
+        }
+
+        while (!q.isEmpty()) {
+            int cur = q.poll();
+            System.out.print(cur + " ");
+            for (int v : graph.get(cur)) {
+                degree[v]--;
+                if (degree[v] == 0) q.add(v);
+            }
+        }
+    }
+}
